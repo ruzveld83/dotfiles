6a42fa3f21175a8f424efc2cd223600dc4cf1a1f
Ruslan Izmaylov <ruslan.izmailov@corp.mail.ru>
2023-12-30 22:18:26 +0300

    Fix nvim current directory

--- .config/nvim/lua/roose/init.lua
+++ .config/nvim/lua/roose/init.lua
@@ -4,3 +4,13 @@ require("roose.lazy")
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.ic = true
+
+-- Make '/path/to/dir' current working directory when starting nvim with
+-- 'nvim /path/to/dir'
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function()
+	local current_netrw_dir = vim.b.netrw_curdir
+	if current_netrw_dir ~= nil and current_netrw_dir ~= '' then
+		vim.api.nvim_set_current_dir(current_netrw_dir)
+	end
+end })
+
