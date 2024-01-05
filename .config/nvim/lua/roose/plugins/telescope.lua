f6bb36be07db2b8d7d3fb4b847e6bb9d1a939b58
Ruslan Izmaylov <ruzveld@gmail.com>
2024-01-06 01:22:59 +0300

    Fix telescope key mapping to enable its loading on startup

--- .config/nvim/lua/roose/plugins/telescope.lua
+++ .config/nvim/lua/roose/plugins/telescope.lua
@@ -1,33 +1,31 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{
-				'nvim-telescope/telescope-fzf-native.nvim',
-				build = 'make',
-				config = function() require("telescope").load_extension("fzf") end
-			},
-			'nvim-tree/nvim-web-devicons'
-		},
-		keys = {
-			{ "<leader>ff", function() require('telescope.builtin').find_files() end },
-			{ "<leader>fg", function() require('telescope.builtin').live_grep() end },
-			{ "<leader>fr", function() require('telescope.builtin').oldfiles() end },
-		},
-		config = function()
-			require("telescope").setup {
-				defaults = {
-					mappings = {
-						i = {
-							["<C-j>"] = "move_selection_next",
-							["<C-k>"] = "move_selection_previous",
-						}
-					}
-				},
-			}
-		end
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                config = function() require("telescope").load_extension("fzf") end,
+            },
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = "move_selection_next",
+                            ["<C-k>"] = "move_selection_previous",
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
-
