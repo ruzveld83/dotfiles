f0fcde6bd257be715716993de590ed1a47aacb06
Ruslan Izmaylov <ruslan.izmailov@corp.mail.ru>
2024-05-25 01:14:20 +0300

    fix nvim build

--- .config/nvim/lua/roose/plugins/debugging.lua
+++ .config/nvim/lua/roose/plugins/debugging.lua
@@ -33,6 +33,7 @@ return {
         "rcarriga/nvim-dap-ui",
         dependencies = {
             "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
         },
         config = function()
             local dap, dapui = require("dap"), require("dapui")
